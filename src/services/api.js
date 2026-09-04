b2258434cf45d32716c876f86522ac4d1adf6908
Osarogie <osarogiedanieligbinosa@gmail.com>
2026-09-04 12:12:00 +0100

    error correction 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api/',
+ baseURL: 'https://business-tracker-hlz7.onrender.com/api/', // Django backend
+ //baseURL: 'http://127.0.0.1:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   }
